39b1ee1587c6ad0bc4d1aa9f74c3a30dfce71fdc
KillA Andrey <andreyka.tryakshin@mail.ru>
2024-05-24 06:35:44 +0700

    Написаны юнит-тесты для тестирования классов бизнес логики, а именно для: ContactTest, PhoneNumberTest, ProjectTest, ProjectTestManager. Все тесты проведены успешно.

--- ContactApp/ContactApp.UnitTests/ProjectManagerTest.cs
+++ ContactApp/ContactApp.UnitTests/ProjectManagerTest.cs
@@ -1,12 +1,63 @@
-﻿using System;
+﻿using ContactsApp;
+using NUnit.Framework;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using Newtonsoft.Json;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework.Legacy;
 
 namespace ContactApp.UnitTests
 {
-    internal class ProjectManagerTest
+    [TestFixture]
+    public class ProjectManagerTest
     {
+        private string testFilePath = @"C:\Users\Андрей\Desktop\ContactApp.txt";
+
+        [SetUp]
+        public void Setup()
+        {
+            if (File.Exists(testFilePath))
+            {
+                File.Delete(testFilePath);
+            }
+        }
+        /// <summary>
+        /// Позитивный тест метода SaveFile
+        /// </summary>
+        [Test(Description = "Позитивный тест метода SaveFile")]
+        public void SaveFile_SuccessfullySavesToFile()
+        {
+            var contactList = new List<Contact>
+            {
+            new Contact("John", "Doe", new DateTime(1990, 1, 1), new PhoneNumber("+79016070939"), "john@example.com", "john_doe"),
+            new Contact("Jane", "Smith", new DateTime(1985, 5, 5), new PhoneNumber("+79016070939"), "jane@example.com", "jane_smith")
+            };
+            var project = new Project(contactList);
+
+            ProjectManager.SaveFile(project);
+
+            ClassicAssert.IsTrue(File.Exists(testFilePath));
+        }
+        /// <summary>
+        /// Позитивный тест метода LoadFile
+        /// </summary>
+        [Test(Description = "Позитивный тест метода LoadFile")]
+        public void LoadFile_SuccessfullyLoadFile()
+        {
+            var contactList = new List<Contact>
+            {
+            new Contact("John", "Doe", new DateTime(1990, 1, 1), new PhoneNumber("+79016070939"), "john@example.com", "john_doe"),
+            new Contact("Jane", "Smith", new DateTime(1985, 5, 5), new PhoneNumber("+79016070939"), "jane@example.com", "jane_smith")
+            };
+            var project = new Project(contactList);
+            ProjectManager.SaveFile(project);
+
+            var loadedProject = ProjectManager.LoadFile();
+            ClassicAssert.IsNotNull(loadedProject);
+            ClassicAssert.AreEqual(contactList.Count, loadedProject.ContactList.Count);
+        }
     }
 }
